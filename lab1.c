c09b2e8984027110417272e3fb7bd32a0015c572
s6704062617016 <s6704062617016@email.kmutnb.ac.th>
2024-11-28 18:36:36 +0700

    lab2 scanf

--- lab1.c
+++ lab1.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
 int main() {
-  int num1=10;
-  int num2=5;
+  int num1;
+  int num2;
+  print("n1:")
+  scarf("%d",&num1)
+  print("n2:")
+  scarf("%d",&num2)
   if (num1 > num2) {
      printf("num1 is greater num2");
+     //diff of num1 and num2
+     printf("\n%d",(num1-num2));
  }else{
-  	printf("num2 is greater num1");
+  	 printf("num2 is greater num1");
+     //diff of num2 and num1
+     printf("\n%d",(num2-num1));
   }
      
  }
