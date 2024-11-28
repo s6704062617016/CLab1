ac4d03ba404f5c5cba3115d4e085a24a48d02564
s6704062617016 <s6704062617016@email.kmutnb.ac.th>
2024-11-28 17:56:06 +0700

    Add files via upload

---
+++ lab1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int num1=10;
+  int num2=5;
+  if (num1 > num2) {
+     printf("num1 is greater num2");
+ }
+  if (num2 > num1){
+  	printf("num2 is greater num1");
+  }
+     
+ }
+
